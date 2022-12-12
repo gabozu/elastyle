4b3976579fbb7317a796289825e47fc408c33028
gabozu <gabriel.j92@gmail.com>
2022-12-11 21:06:22 -0500

    cambios menores

--- static/js/loginSigin.js
+++ static/js/loginSigin.js
@@ -1,3 +1,8 @@
+var oracledb = require('oracledb');
+const { listenerCount } = require('process');
+oracledb.autoCommit = true;
+const prompt = require('prompt-sync')();
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -9,3 +14,30 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+function validation (){
+	getButtonValue = document.getElementByClassName("email-input");
+	console.log(getButtonValue.value);
+}
+
+var contador=0;
+const config = {
+  user: 'USUARIO6',
+  password: 'USUARIO6',
+  connectString: "localhost/xe" // nombre del host 
+}
+
+async function getUsers () {
+  let con;
+  try {
+    con = await oracledb.getConnection(config)
+	if(con){
+		return 0 ;
+	}
+  }catch (err){
+    console.error(err);
+  }
+  await con.close();
+}
+
+getUsers();
